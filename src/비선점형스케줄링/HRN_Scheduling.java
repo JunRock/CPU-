66be35774bbab1c49dd1dff11862f4bca5cb1681
leejunseok <wnstjr120422@naver.com>
2023-04-30 01:39:22 +0900

    HRN스케줄링 구현완료

--- src/비선점형스케줄링/HRN_Scheduling.java
+++ src/비선점형스케줄링/HRN_Scheduling.java
@@ -11,7 +11,7 @@ public class HRN_Scheduling {
         BufferedReader br = null;
         String processId;
         Deque<String> q = new LinkedList<>();
-        Queue<String>tmp_q=new LinkedList<>();
+        Deque<String>tmp_q=new LinkedList<>();
         double[] arrtime = new double[5];
         double arriveTime, serviceTime, retime; //프로세스ID, 도착시간, 작업시간, 반환시간
 
@@ -33,11 +33,12 @@ public class HRN_Scheduling {
         }
         String tmp_processId;
         double tmp_servicetime,tmp_arrivetime;
-        int sum = 0,tmp_sum=0;
+        double sum = 0;
+        double tmp_sum=0;
         double arrsum = 0,tmp_arrsum=0; //프로세스 대기시간 총합
         int resum = 0;
         double re_time_sum = 0;
-        int[] time = new int[5]; //프로세스 별 반환시간 저장배열
+        double[] time = new double[5]; //프로세스 별 반환시간 저장배열
         int []priority=new int[5];
         /*
         도착시간이 0인 프로세스 탐색
@@ -51,36 +52,39 @@ public class HRN_Scheduling {
                 st.nextToken();
                 st.nextToken();
                 if (arriveTime == 0) {
-                    q.add(process[i]);
-                    tmp_q.add(process[i]);
+                    q.addLast(process[i]);
+                    tmp_q.addLast(process[i]);
                 }
             }
         }
         /*
         덱에서 하나씩 꺼내서 HRN우선순위 판별
          */
-        String []tmp=new String[1];
+        String tmp = null;
         for(int i=0;i< process.length;i++){
-            StringTokenizer str=new StringTokenizer(q.peekLast());
+            String que=q.peekLast();
+            StringTokenizer str=new StringTokenizer(que);
             double max_priority=0;
+
             while(str.hasMoreTokens()){
                 tmp_processId=str.nextToken();
-                tmp_servicetime=Integer.parseInt(str.nextToken());
-                tmp_arrivetime=Integer.parseInt(str.nextToken());
+                tmp_arrivetime=Double.parseDouble(str.nextToken());
+                tmp_servicetime=Double.parseDouble(str.nextToken());
                 sum += tmp_servicetime; //실행시간 총합
                 str.nextToken();str.nextToken();
             }
 
-            for(int k=0;k< process.length;k++){
+            for(int k=0;k<process.length;k++){
                 int count=0;
                 StringTokenizer st1=new StringTokenizer(process[k]);
                 while(st1.hasMoreTokens()){
                     processId=st1.nextToken();
-                    arriveTime=Integer.parseInt(st1.nextToken());
-                    serviceTime=Integer.parseInt(st1.nextToken());
+                    arriveTime=Double.parseDouble(st1.nextToken());
+                    serviceTime=Double.parseDouble(st1.nextToken());
                     /*
                     이미 큐 안에 동일한 프로세스가 들어가 있는지 확인
                      */
+                     //덱에 존재하는 프로세스들 확인
                     Iterator it= q.iterator();
                     while(it.hasNext()){
                         String s= (String) it.next();
@@ -90,22 +94,58 @@ public class HRN_Scheduling {
                             if(Id.equals(processId))
                                 count++;
                         }
+
                     }
                     if(count==0) { //실제 우선순위를 구하는 작업을 하는 조건문
-                            Math.max(max_priority,(serviceTime+(sum-arriveTime))/serviceTime);
-                            tmp[0]=process[k];
+                        double m=(serviceTime+(sum-arriveTime))/serviceTime;
+                        if(max_priority<m){
+                            max_priority=Math.max(max_priority,m);
+                            tmp=process[k];
+                        }
                     }
                     st1.nextToken();st1.nextToken();
                 }
             }
-            q.addLast(tmp[0]);
-            tmp_q.add(tmp[0]);
+            q.addLast(tmp);
+            tmp_q.add(tmp);
+        }
+        tmp_q.pollLast(); //마지막 요소 삭제
+        sum=0;
+        int j = 0;
+        Iterator it = tmp_q.iterator();
+        while (it.hasNext()) {
+            String str = (String) it.next();
+            StringTokenizer st = new StringTokenizer(str);
+            while (st.hasMoreTokens()) {
+                System.out.print(st.nextToken() + "의 대기시간: "); //프로세스 ID
+                arriveTime = Integer.parseInt(st.nextToken()); //도착시간
+                System.out.println(sum - arriveTime);
+                arrsum += (sum - arriveTime);
+                serviceTime = Integer.parseInt(st.nextToken()); //실행시간
+                sum += serviceTime;
+                time[j] = (sum - arriveTime);
+                j++;
+                st.nextToken(); //우선순위
+                st.nextToken(); //시간 할당량
+            }
         }
 
-        Iterator it=tmp_q.iterator();
-        while(it.hasNext()){
-            System.out.println(it.next());
+        System.out.println("평균 대기 시간(AWT): " + arrsum / process.length);
+        j=0;
+        it = tmp_q.iterator();
+        while (it.hasNext()) {
+            String str = (String) it.next();
+            StringTokenizer st = new StringTokenizer(str);
+            while (st.hasMoreTokens()) {
+                System.out.println(st.nextToken() + "의 반환시간: " + time[j]); //프로세스 ID
+                st.nextToken();
+                st.nextToken();
+                st.nextToken(); //우선순위
+                st.nextToken(); //시간 할당량
+            }
+            re_time_sum += time[j++];
         }
+        System.out.println("평균 반환 시간(ATT): "+re_time_sum /process.length);
     }
 
     public static void main(String[] args) {
