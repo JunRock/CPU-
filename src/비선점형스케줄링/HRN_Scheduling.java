c705bd9578ea1dce530de815ecbbd4f0f298d195
leejunseok <wnstjr120422@naver.com>
2023-04-29 22:49:18 +0900

    HRN스케줄링 구현

--- src/비선점형스케줄링/HRN_Scheduling.java
+++ src/비선점형스케줄링/HRN_Scheduling.java
@@ -10,7 +10,8 @@ public class HRN_Scheduling {
         FileReader fin = null;
         BufferedReader br = null;
         String processId;
-        Queue<String> q = new LinkedList<>();
+        Deque<String> q = new LinkedList<>();
+        Queue<String>tmp_q=new LinkedList<>();
         double[] arrtime = new double[5];
         double arriveTime, serviceTime, retime; //프로세스ID, 도착시간, 작업시간, 반환시간
 
@@ -31,11 +32,8 @@ public class HRN_Scheduling {
             e.printStackTrace();
         }
 
-        int[] tmp_servicetime = new int[5];
-        int[] tmp_arrivetime=new int[5];
-        String[] tmp_processId=new String[5];
-        int sum = 0;
-        double arrsum = 0; //프로세스 대기시간 총합
+        int sum = 0,tmp_sum=0;
+        double arrsum = 0,tmp_arrsum=0; //프로세스 대기시간 총합
         int resum = 0;
         double re_time_sum = 0;
         int[] time = new int[5]; //프로세스 별 반환시간 저장배열
@@ -49,21 +47,17 @@ public class HRN_Scheduling {
                 processId=st.nextToken();
                 arriveTime=Double.parseDouble(st.nextToken());
                 serviceTime=Double.parseDouble(st.nextToken());
+                st.nextToken();st.nextToken();
                 if(arriveTime==0)
                     q.add(process[i]);
+                //tmp_q.add(process[i]);
             }
         }
+        /*
+        덱에서 하나씩 꺼내서 HRN우선순위 판별
+         */
+        for(int i=0;i<process.length;i++) {
 
-        for(int i=0;i<process.length;i++){
-            String str=q.peek();
-            StringTokenizer st1=new StringTokenizer(str);
-            int wait_time;
-            while(st1.hasMoreTokens()){
-                processId=st1.nextToken();
-                arriveTime=Double.parseDouble(st1.nextToken());
-                serviceTime=Double.parseDouble(st1.nextToken());
-
-            }
         }
     }
 
