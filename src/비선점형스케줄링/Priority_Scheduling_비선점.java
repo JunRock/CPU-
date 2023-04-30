561b204fb847dfa19ba2459daa1067dbb6a12f4f
leejunseok <wnstjr120422@naver.com>
2023-04-30 15:11:20 +0900

    비선점우선순위 스케줄링 구현

---
+++ src/비선점형스케줄링/Priority_Scheduling_비선점.java
@@ -0,0 +1,12 @@
+package 비선점형스케줄링;
+
+public class Priority_Scheduling_비선점 {
+    public void run(){
+
+    }
+
+    public static void main(String[] args) {
+        Priority_Scheduling_비선점 priority_scheduling_비선점=new Priority_Scheduling_비선점();
+        priority_scheduling_비선점.run();
+    }
+}
